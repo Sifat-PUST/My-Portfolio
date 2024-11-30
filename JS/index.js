ab1579750757ddfd88b606761093dab047a29f72
Md.Sifat <115576448+Sifat-PUST@users.noreply.github.com>
2024-12-01 00:54:27 +0600

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -8,12 +8,12 @@ hamberger.addEventListener('click',(event)=>{
         // console.log('Hello')
         hamberger.classList.replace('ri-menu-5-line','ri-close-fill')
         nav_links_div.classList.replace('right-[-300px]','right-0')
-        // nav_links_div.classList.replace('hidden','block')
+        nav_links_div.classList.replace('hidden','block')
     }
     else{
         hamberger.classList.replace('ri-close-fill','ri-menu-5-line')
         nav_links_div.classList.replace('right-0','right-[-300px]')
-        // nav_links_div.classList.replace('block','hidden')
+        nav_links_div.classList.replace('block','hidden')
     }
     // nav_links_div.classList.toggle('right-0')
 })
